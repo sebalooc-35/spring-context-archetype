72a8d5c419ab4bf172cbb7caaa24b498126d7353
sebalooc35 <andres.ordonez@galileo.edu>
2025-03-08 19:53:47 -0600

    fix beans scanner structure

--- src/main/resources/archetype-resources/src/main/java/config/MainConfig.java
+++ src/main/resources/archetype-resources/src/main/java/config/MainConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "com.example.beans")
+@ComponentScan(basePackages = "${package}.beans")
 public class MainConfig {
 
     @Bean(name = "hello")
